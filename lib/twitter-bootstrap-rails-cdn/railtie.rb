337e53abb65a2b27edff681afc3f0c34c77cd5ae
Dom <hello@meetdom.com>
2013-10-12 13:11:15 +1000

    Expand module namespacing to counter concatenated TwitterBootstrap.

--- lib/twitter-bootstrap-rails-cdn/railtie.rb
+++ lib/twitter-bootstrap-rails-cdn/railtie.rb
@@ -1,8 +1,12 @@
-module TwitterBootstrap::Rails::Cdn
-  class Railtie < ::Rails::Railtie
-    initializer 'twitter_bootstrap_rails_cdn.action_view' do |app|
-      ActiveSupport.on_load(:action_view) do
-        include TwitterBootstrap::Rails::Cdn::ActionViewExtensions
+module TwitterBootstrap
+  module Rails
+    module Cdn
+      class Railtie < ::Rails::Railtie
+        initializer 'twitter_bootstrap_rails_cdn.action_view' do |app|
+          ActiveSupport.on_load(:action_view) do
+            include TwitterBootstrap::Rails::Cdn::ActionViewExtensions
+          end
+        end
       end
     end
   end
